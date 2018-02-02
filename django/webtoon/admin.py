16505cce0926b769f772165afadcb0d4eb379d98
Shin Dongjin <djshin2000@gmail.com>
2018-02-03 00:18:55 +0900

    webtoon App의 model 및 admin 작성

--- django/webtoon/admin.py
+++ django/webtoon/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Webtoon, Episode
 
-# Register your models here.
+admin.site.register(Webtoon)
+admin.site.register(Episode)
--- django/webtoon/models.py
+++ django/webtoon/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Webtoon(models.Model):
+    webtoon_id = models.CharField(max_length=15)
+    title = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f'{self.title} | {self.webtoon_id}'
+
+
+class Episode(models.Model):
+    webtoon = models.ForeignKey('Webtoon', on_delete=models.CASCADE)
+    episode_id = models.CharField(max_length=10)
+    title = models.CharField(max_length=200)
+    rating = models.CharField(max_length=5)
+    created_date = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.title} | {self.episode_id}'
